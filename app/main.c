a7e5349bb9fd3d1ea998f6d92451e779e9d392f2
nardoz-dev <nardomen96@gmail.com>
2023-05-15 17:48:31 +0200

    integrate branch

--- app/main.c
+++ app/main.c
@@ -6,30 +6,7 @@
 #include "dht_params.h"
 #include "periph/gpio.h"
 
-
-//Pin for the display 7 segment.
-/* OLD CONFIGURATION 
-gpio_t segment_a = GPIO_PIN(PORT_A,9);   
-gpio_t segment_b = GPIO_PIN(PORT_B,8);
-gpio_t segment_c = GPIO_PIN(PORT_A,5);
-gpio_t segment_d = GPIO_PIN(PORT_A,2);
-gpio_t segment_e = GPIO_PIN(PORT_A,10);
-gpio_t segment_f = GPIO_PIN(PORT_B,3);
-gpio_t segment_g = GPIO_PIN(PORT_B,9);
-gpio_t segment_h = GPIO_PIN(PORT_A,3);
-gpio_t digit = GPIO_PIN(PORT_C,7);
-*/
-
-/*
-New configuration
-A -> D6
-B -> D3
-C -> D11
-D -> D10
-E -> D2
-F -> D4
-G -> D5
-*/
+/* GPIO pin for 7Segment Display*/
 gpio_t SEGMENT_A = GPIO_PIN(PORT_B,10);   
 gpio_t SEGMENT_B = GPIO_PIN(PORT_B,3);
 gpio_t SEGMENT_C = GPIO_PIN(PORT_A,7);
@@ -39,22 +16,15 @@ gpio_t SEGMENT_F = GPIO_PIN(PORT_B,5);
 gpio_t SEGMENT_G = GPIO_PIN(PORT_B,4);
 
 // Array di valori per le singole cifre del display
-// Array di valori per le singole cifre del display
-static const uint8_t digit_values[][7] = {
+static const uint8_t digit_values[][4] = {
     {0, 0, 0, 0, 0, 0, 1}, // 0
-    {1, 0, 0, 1, 1, 1, 1}, // 1    /* OSS IMPORTANTE WORKA SOLO 0-1 CHECK SU ALTRI NUMERI*/
+    {1, 0, 0, 1, 1, 1, 1}, // 1    
     {0, 0, 1, 0, 0, 1, 0}, // 2
-    {0, 0, 0, 0, 1, 1, 0}, // 3
-    {0, 1, 1, 0, 0, 1, 1}, // 4
-    {1, 0, 1, 1, 0, 1, 1}, // 5
-    {1, 0, 1, 1, 1, 1, 1}, // 6
-    {1, 1, 1, 0, 0, 0, 0}, // 7
-    {1, 1, 1, 1, 1, 1, 1}, // 8
-    {1, 1, 1, 1, 0, 1, 1}, // 9
+    {0, 0, 0, 0, 1, 1, 0}, // 3         
 };
 
 
-//Inizializza il display
+//Display initialization
 void init_display(void) {
     gpio_init(SEGMENT_A, GPIO_OUT);
     gpio_init(SEGMENT_B, GPIO_OUT);
@@ -66,9 +36,8 @@ void init_display(void) {
 }
 
 
-// Imposta il valore di una singola cifra del display
+// Set value on display
 void set_digit_value(int value) {
-
     const uint8_t* segment_values = digit_values[value];
     gpio_write(SEGMENT_A, segment_values[0]);
     gpio_write(SEGMENT_B, segment_values[1]);
@@ -77,49 +46,12 @@ void set_digit_value(int value) {
     gpio_write(SEGMENT_E, segment_values[4]);
     gpio_write(SEGMENT_F, segment_values[5]);
     gpio_write(SEGMENT_G, segment_values[6]);
-    
 }
 
 
 
 int main(void){
-    
-    /*
-    xtimer_sleep(2);
-    gpio_t motor_pin = GPIO_PIN(PORT_C,7);
-    if (gpio_init(motor_pin, GPIO_OUT)) {
-        printf("Error to initialize GPIO_PIN(%d %d)\n", PORT_C, 7);
-        return -1;
-    }
-    else {
-        printf("Good initialization\n");
-    }
 
-    gpio_t led_pin = GPIO_PIN(PORT_A,9);
-    if (gpio_init(led_pin, GPIO_OUT)) {
-        printf("Error to initialize GPIO_PIN(%d %d)\n", PORT_A, 9);
-        return -1;
-    }
-    else {
-        printf("Good initialization\n");
-    }
-    */
-    /*
-    while(1){
-        xtimer_sleep(5);
-        printf("Set motor On\n");
-        gpio_set(motor_pin);
-        //gpio_set(led_pin);
-        xtimer_sleep(5);
-
-        printf("Set motor Off\n");
-        gpio_clear(motor_pin);
-        //gpio_clear(led_pin);
-
-    }
-    */
-    
-    
     printf("RIOT windforme application\n"
            "AirCooler Test Application\n"
            "using RIOT DHT peripheral driver and Motor Mabuchi FC130\n"
@@ -128,7 +60,7 @@ int main(void){
     //Initialize display pin
     init_display();
     set_digit_value(0);
-    /*
+    
 
     // Fix port parameter for digital sensor 
     dht_params_t my_params;
@@ -185,12 +117,14 @@ int main(void){
          // Check temperature and activate motor if necessary 
         if (((unsigned int)temp >= 255) && (!flag) && (!controlflag)) {
             printf("Temperature above threshold - activating motor\n");
+            set_digit_value(1);
             gpio_set(motor_pin);
             flag = true;
         }
         if(((unsigned int)temp <= 254) && flag && (!controlflag)){
             xtimer_sleep(2);
             printf("Temperature under threshold - deactivating motor \n");
+            set_digit_value(0)
             gpio_clear(motor_pin);
             flag = false;
             xtimer_sleep(5);
@@ -208,6 +142,6 @@ int main(void){
         //Clock Time for picking samples
         xtimer_sleep(3);
     }
-    */
+    
     return 0;
 }
