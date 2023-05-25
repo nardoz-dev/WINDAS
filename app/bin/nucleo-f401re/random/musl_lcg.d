f5b3ad7c76a7d3637cf338e42b109ba6ed5076c4
nardoz-dev <nardomen96@gmail.com>
2023-05-26 01:20:31 +0200

    little fix

--- app/bin/nucleo-f401re/random/musl_lcg.d
+++
@@ -1,8 +0,0 @@
-/home/nardo/Desktop/WINDFORME/app/bin/nucleo-f401re/random/musl_lcg.o: \
- /home/nardo/Desktop/RIOT/sys/random/musl_lcg.c \
- /home/nardo/Desktop/WINDFORME/app/bin/nucleo-f401re/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h
-
-/home/nardo/Desktop/WINDFORME/app/bin/nucleo-f401re/riotbuild/riotbuild.h:
-
-/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
